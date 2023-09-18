4f610fc14faab58fb59b004c94f35993abca773f
Gustavo <gustavohdlima@hotmail.com>
2023-09-18 14:31:47 -0300

    Test: Testes unitários - pizza-query-failed-error-filter

--- src/filters/pizza-query-failed-error/pizza-query-failed-error.filter.spec.ts
+++ src/filters/pizza-query-failed-error/pizza-query-failed-error.filter.spec.ts
@@ -1,7 +1,33 @@
+import { QueryFailedError } from 'typeorm';
 import { PizzaQueryFailedErrorFilter } from './pizza-query-failed-error.filter';
+import { argumentsHostMock } from 'testing-mocks/arguments-host.mock';
+import { httpArgumentsHostMock } from 'testing-mocks/http-arguments-host.mock';
 
 describe('PizzaQueryFailedErrorFilter', () => {
+  const filter: PizzaQueryFailedErrorFilter<QueryFailedError> = new PizzaQueryFailedErrorFilter();
+
+  beforeEach(() => {
+    httpArgumentsHostMock.mockClear();
+  });
+
   it('should be defined', () => {
-    expect(new PizzaQueryFailedErrorFilter()).toBeDefined();
+    expect(filter).toBeDefined();
+  });
+
+  it('should call be case code 23505 - user already exists', () => {
+    filter.catch(
+      new QueryFailedError('', [], { code: '23505', detail: 'error filter test' }),
+      argumentsHostMock,
+    );
+    expect(filter.code).toEqual('23505');
+    expect(httpArgumentsHostMock).toBeCalledWith();
+    expect(httpArgumentsHostMock).toBeCalledTimes(2);
+  });
+
+  it('should call be case default - any other case', () => {
+    filter.catch(new QueryFailedError('', [], { detail: 'error filter test' }), argumentsHostMock);
+    expect(filter.code).toEqual('500');
+    expect(httpArgumentsHostMock).toBeCalledWith();
+    expect(httpArgumentsHostMock).toBeCalledTimes(2);
   });
 });
