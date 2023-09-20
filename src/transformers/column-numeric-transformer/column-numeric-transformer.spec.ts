213c00db73014a28492a07f905e777345167ae6b
Gustavo <gustavohdlima@hotmail.com>
2023-09-20 15:00:03 -0300

    Test: Testes unitários - column-numeric-transformer

--- src/transformers/column-numeric-transformer/column-numeric-transformer.spec.ts
+++ src/transformers/column-numeric-transformer/column-numeric-transformer.spec.ts
@@ -1,7 +1,18 @@
 import { ColumnNumericTransformer } from './column-numeric-transformer';
 
 describe('ColumnNumericTransformer', () => {
+  let transformer: ColumnNumericTransformer;
+
+  beforeEach(() => {
+    transformer = new ColumnNumericTransformer();
+  });
+
   it('should be defined', () => {
-    expect(new ColumnNumericTransformer()).toBeDefined();
+    expect(transformer).toBeDefined();
+  });
+
+  it('should converter string to number', () => {
+    expect(typeof transformer.from('2')).toBe('number');
+    expect(transformer.from('2')).toBe(2);
   });
 });
