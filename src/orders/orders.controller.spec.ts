aa576019dce02e8af8b3ca3c1d43d33a45dbe586
Gustavo <gustavohdlima@hotmail.com>
2023-09-20 12:40:42 -0300

    Test: Testes unitários - orders.controller
    E criando mock necessário

--- src/orders/orders.controller.spec.ts
+++ src/orders/orders.controller.spec.ts
@@ -1,4 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ordersMock } from 'testing-mocks/orders.mock';
+import { ordersServiceMock } from 'testing-mocks/orders.service.mock';
+import { requestExpressMock } from 'testing-mocks/request-express.mock';
 import { OrdersController } from './orders.controller';
 
 describe('OrdersController', () => {
@@ -7,6 +10,7 @@ describe('OrdersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [OrdersController],
+      providers: [ordersServiceMock],
     }).compile();
 
     controller = module.get<OrdersController>(OrdersController);
@@ -14,5 +18,10 @@ describe('OrdersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return all orders from the user logged in', async () => {
+    jest.spyOn(ordersServiceMock.useValue, 'findAll').mockReturnValue([ordersMock]);
+    expect(await controller.findAll(requestExpressMock)).toEqual([ordersMock]);
   });
 });
---
+++ testing-mocks/orders.mock.ts
@@ -0,0 +1,4 @@
+export const ordersMock = {
+  id: 'e1ec77d7-efcf-4d93-b7ec-af2f3ac88c73',
+  paidOff: false,
+};
