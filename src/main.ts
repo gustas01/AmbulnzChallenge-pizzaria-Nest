561c84147cb988661fc85cd4bdcf8816553f4bb0
Gustavo <gustavohdlima@hotmail.com>
2023-09-22 13:29:42 -0300

    Feat: Adicionando valição estrita no pipe, para os dtos ignorarem chaves que não estão aptos à receberem

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
   await app.listen(3000);
 }
