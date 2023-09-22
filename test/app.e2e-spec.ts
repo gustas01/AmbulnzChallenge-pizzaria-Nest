0b1f8dd60f0b72f8105533a447c40bed83c7217b
Gustavo <gustavohdlima@hotmail.com>
2023-09-22 17:23:59 -0300

    Test: Testes E2E - Criando usuário e logando

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,15 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { User } from 'src/users/entities/user';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { userDataMock } from 'testing-mocks/user-data.mock';
+import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let token: string;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +18,32 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should create a user', async () => {
+    const response = await request(app.getHttpServer()).post('/auth/register').send(userDataMock);
+    const body = response.body as User;
+
+    expect(body?.username).toEqual(userDataMock.username);
+    expect(body?.name).toEqual(userDataMock.name);
+    expect(body?.roles).toEqual(userDataMock.roles);
+    expect(response.statusCode).toEqual(201);
+  });
+
+  it('should signIn', async () => {
+    const response = await request(app.getHttpServer()).post('/auth/login').send(userDataMock);
+
+    const setCookieContent: string[] = response.header['set-cookie'];
+
+    token = setCookieContent
+      .find((el) => el.startsWith('token='))
+      ?.split('=')[1]
+      ?.split(';')[0];
+
+    expect(token).toBeDefined();
+    expect(typeof token).toEqual('string');
+    expect(response.statusCode).toEqual(200);
   });
 });
